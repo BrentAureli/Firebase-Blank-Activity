e454b4753845f1cde23ed1abb7e8795b4b4dd939
Brent Aureli <Brent Aureli>
2016-08-09 01:52:03 -0700

    initial

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Firebase-Blank-Activity
 Configured Firebase ready to run, just provide your google-services.json file!
+
+Configured files:
+Project Gradle File
+App level Gradle File
---
+++ app/src/main/java/com/brentaureli/firebase/firebasebasic/MainActivity.java
@@ -0,0 +1,23 @@
+package com.brentaureli.firebase.firebasebasic;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ui.email.SignInActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
